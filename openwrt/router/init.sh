33ee3030db57bae4e6de2446c1b761598a8fe2b6
Bryan Cuccioli <842035+bcuccioli@users.noreply.github.com>
2020-11-19 21:24:21 -0500

    Add mesh/batman packages for router

--- openwrt/router/init.sh
+++ openwrt/router/init.sh
@@ -4,3 +4,6 @@ opkg update
 opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade
 
 opkg install luci-ssl luci-app-nlbwmon
+
+opkg remove wpad-basic
+opkg install kmod-batman-adv wpad-mesh-openssl
