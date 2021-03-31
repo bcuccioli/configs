b4b2df949106bdfdb5ac2d157e907f9213ea91bc
Bryan Cuccioli <842035+bcuccioli@users.noreply.github.com>
2021-03-31 15:26:38 -0400

    Clean up Firefox user.js
    * Remove deprecated settings.
    * Remove settings which are controlled via settings UI.
    * Add a couple of others.

--- firefox/user.js
+++ firefox/user.js
@@ -1,91 +1,38 @@
 user_pref("app.normandy.api_url", "");
 user_pref("app.normandy.enabled", false);
-user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("beacon.enabled", false);
 user_pref("breakpad.reportURL", "");
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit", false);
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
-user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
-user_pref("browser.download.manager.retention", 0);  // Do not retain download history.
-user_pref("browser.fixup.alternate.enabled", false);
-user_pref("browser.newtabpage.enabled", false);
-user_pref("browser.newtabpage.enhanced", false);
-user_pref("browser.newtabpage.introShown", true);
-user_pref("browser.safebrowsing.appRepURL", "");
-user_pref("browser.safebrowsing.blockedURIs.enabled", false);
-user_pref("browser.safebrowsing.downloads.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
-user_pref("browser.safebrowsing.downloads.remote.url", "");
-user_pref("browser.safebrowsing.enabled", false);
-user_pref("browser.safebrowsing.malware.enabled", false);
-user_pref("browser.safebrowsing.phishing.enabled", false);
-user_pref("browser.search.geoip.uri", "");
-user_pref("browser.search.suggest.enabled", false);
-user_pref("browser.selfsupport.url", "");
-user_pref("browser.send_pings", false);
-user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.sessionstore.privacy_level", 2);
 user_pref("browser.tabs.crashReporting.sendReport", false);
-user_pref("browser.toolbarbuttons.introduced.pocket-button", true);
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("browser.urlbar.trimURLs", false);
-user_pref("datareporting.healthreport.service.enabled", false);
-user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
-user_pref("dom.allow_scripts_to_close_windows", true);  // Needed for something I was developing.
 user_pref("dom.battery.enabled", false);
 user_pref("dom.webaudio.enabled", false);
-user_pref("experiments.activeExperiment", false);
-user_pref("experiments.enabled", false);
-user_pref("experiments.manifest.uri", "");
-user_pref("experiments.supported", false);
 user_pref("extensions.getAddons.cache.enabled", false);
-user_pref("extensions.getAddons.showPane", false);
 user_pref("extensions.screenshots.disabled", true);
 user_pref("extensions.screenshots.upload-disabled", true);
-user_pref("extensions.shield-recipe-client.api_url", "");
-user_pref("extensions.shield-recipe-client.enabled", false);
-user_pref("extensions.webservice.discoverURL", "");
 user_pref("geo.enabled", false);
-user_pref("general.warnOnAboutConfig", false);
-user_pref("layout.css.visited_links_enabled", false);  // Disable visited link colors.
-user_pref("media.autoplay.default", 0);
-user_pref("media.autoplay.enabled", true);
+user_pref("layout.css.visited_links_enabled", false);
 user_pref("media.navigator.enabled", false);
 user_pref("media.peerconnection.enabled", false);  // Disable WebRTC.
 user_pref("media.peerconnection.ice.default_address_only", true);
 user_pref("media.video_stats.enabled", false);
-user_pref("network.allow-experiments", false);
 user_pref("network.captive-portal-service.enabled", false);
-user_pref("network.cookie.cookieBehavior", 1);  // Accept only first-party domain cookies.
 user_pref("network.cookie.lifetimePolicy", 3);  // Keep cookies for 30 days.
 user_pref("network.cookie.lifetime.days", 30);
-user_pref("network.dns.disablePrefetch", true);
 user_pref("network.http.referer.trimmingPolicy", 2);  // Send referrer only when domain matches.
 user_pref("network.http.referer.XOriginPolicy", 2);
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
-user_pref("network.http.speculative-parallel-limit", 0);
-user_pref("network.prefetch-next", false);
-user_pref("network.security.esni.enabled", true);  // Enable encrypted SNI.
-user_pref("network.trr.mode", 2);
 user_pref("privacy.firstparty.isolate", true);
-user_pref("privacy.trackingprotection.enabled", true);
-user_pref("privacy.trackingprotection.pbmode.enabled", true);
 user_pref("security.osclientcerts.autoload", true);  // For accessing work internals off-VPN.
-user_pref("security.ssl.disable_session_identifiers", true);
-user_pref("signon.autofillForms", false);
 user_pref("toolkit.telemetry.archive.enabled", false);
 user_pref("toolkit.telemetry.bhrPing.enabled", false);
-user_pref("toolkit.telemetry.cachedClientID", "");
-user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
-user_pref("toolkit.telemetry.hybridContent.enabled", false);
 user_pref("toolkit.telemetry.newProfilePing.enabled", false);
-user_pref("toolkit.telemetry.prompted", 2);
-user_pref("toolkit.telemetry.rejected", true);
-user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 user_pref("toolkit.telemetry.server", "");
 user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
 user_pref("toolkit.telemetry.unified", false);
-user_pref("toolkit.telemetry.unifiedIsOptIn", false);
 user_pref("toolkit.telemetry.updatePing.enabled", false);
 user_pref("webgl.disabled", true);
