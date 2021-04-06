41af5f9a4e42acd4f743648e3cb23bf5bc1859da
Bryan Cuccioli <me@bryanlc.com>
2021-04-06 19:10:22 -0400

    Simplify structure of OpenWrt configs
    We no longer have nodes, just the main router. This commit removes
    the node configs and promotes the router configs up one directory
    level. In practice these will be deployed on node hardware.
    
    There may be some changes coming to the configs in the next
    commit.

--- openwrt/router/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-opkg update
-opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade
-
-opkg install luci-ssl luci-app-nlbwmon
-
-opkg remove wpad-basic
-opkg install kmod-batman-adv wpad-mesh-openssl
