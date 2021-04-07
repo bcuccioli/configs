8ecea02a5e2e87688e992376d8051dff10c4999c
Bryan Cuccioli <me@bryanlc.com>
2021-04-06 20:46:02 -0400

    Update OpenWrt router configs
    This is mainly simplification because we no longer have a mesh
    network.

--- openwrt/init.sh
+++ openwrt/init.sh
@@ -1,44 +1,17 @@
 #!/bin/sh
 
-random_key() {
-    tr -dc A-NP-Za-km-z2-9 </dev/urandom | head -c ${1:-16}
-}
+opkg update;
+opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade;
 
-echo "Setting hostname";
-uci set system.@system[0].hostname="mesh-node-$(random_key 4)";
-
-echo "Basic configuration";
-uci set network.lan.gateway="192.168.1.1";
-uci set network.lan.ipaddr="192.168.1.4";
-uci add_list dhcp.@dnsmasq[0].server="192.168.1.1";
-uci set batmand.general.interface="bat0";
-uci set dhcp.lan.ignore=1;
-
-echo "Removing unneeded things";
 rm /etc/config/fm;
 rm -rf /freemesh/;
 /etc/init.d/fmstartup disable;
 rm /etc/init.d/fmstartup;
 
-/etc/init.d/firewall disable;
-/etc/init.d/dnsmasq disable;
 /etc/init.d/uhttpd disable;
-rm /etc/init.d/firewall;
-rm /etc/init.d/dnsmasq;
 rm /etc/init.d/uhttpd;
-rm /etc/config/firewall;
-rm /etc/firewall.user;
-rm /etc/config/dhcp;
-rm /etc/config/uhttpd;
 
-echo "Committing";
-uci commit;
-
-# Set Wifi LEDs to be on and steady.
-echo "Setting LEDs";
-echo "none" > /sys/class/leds/mt76-phy0/trigger;
-echo "none" > /sys/class/leds/zbt-we826\:green\:wifi/trigger;
-echo 1 > /sys/class/leds/mt76-phy0/brightness;
-echo 1 > /sys/class/leds/zbt-we826\:green\:wifi/brightness;
+/etc/init.d/batmand disable;
+rm /etc/init.d/batmand;
 
 reboot;
---
+++ openwrt/upgrade.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade
